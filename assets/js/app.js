0d62af359d385e97bd7d32a0cf8d222fd18b45e1
MthAdam <96947337+MthAdam@users.noreply.github.com>
2022-03-12 00:13:43 +0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+//-- slider script start//
+
+$(function(){
+    $('.banner_slider').slick({
+        prevArrow:'<i class=" banner_slider_icon fas fa-long-arrow-alt-left"></i>',
+        nextArrow:'<i class=" banner_slider_icon fas fa-long-arrow-alt-right"></i>',
+        dots:true,
+        fade:true,
+    });
+  });
+
+// slider script end--//
